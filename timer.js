d4ba8a98f79346ec25dee5ece62925fb762bfa1b
Katie Adaline Sroub <ksroub@gmail.com>
2017-12-03 17:34:18 -0500

    Creating buttons and time intervals

--- timer.js
+++ timer.js
@@ -1,3 +1,22 @@
+/*window.palmadoTimer = Timer();
+
+MyTimer = {
+	duration: 600,
+	reset: function() {
+		this.duration = 0;
+	},
+	setDuration: function(newDuration) {
+		this.duration = newDuration
+	},
+	startTimer() {
+		
+	}
+}
+
+MyTimer.reset()
+*/
+
+
 function startTimer(duration, display){
 	var timer = duration, minutes, seconds;
 	var theTimer = setInterval(function(){
@@ -7,7 +26,7 @@ function startTimer(duration, display){
 		minutes = minutes < 10 ? "0" + minutes : minutes;
 		seconds = seconds < 10 ? "0" + seconds : seconds;
 		
-		display.textContent = "Keep going! You have " + minutes + ":" + seconds + " minutes remaining.";
+		display.textContent = "You have " + minutes + ":" + seconds + " minutes remaining.";
 			
 		
 		if (--timer < 0){
@@ -18,12 +37,49 @@ function startTimer(duration, display){
 	}, 1000);
 	
 }
-	
 
+function reset(duration, display) {
+clearInterval	
+}
+/*
 
 window.onload = function () {
 	var twentyFiveMinutes = 60 * 25,
 		display = document.querySelector('#time');
 	startTimer(twentyFiveMinutes, display);
 	
+}; 
+
+
+//var startButton = document.getElementById('startButton');
+*/
+
+
+//window.document.getElementById('startButton').onclick = 
+
+function startPomodoroz() {
+	var twentyFiveMinutes = 60 * 25,
+		display = document.querySelector('#time');
+	startTimer(twentyFiveMinutes, display);
+	
 };
+
+
+function startShortBreak(){
+	var fiveMinutes = 6 * 1,
+		display = document.querySelector('#time');
+	startTimer(fiveMinutes, display);
+};
+
+function startLongBreak(){
+	var tenMinutes = 60 *10,
+		display = document.querySelector('#time');
+	startTimer(tenMinutes, display);
+};
+
+function reset(){
+	clearInterval(theTimer);
+	display = document.querySelector('#time');
+	startTimer(display);
+};
+
